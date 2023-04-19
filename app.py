245765ed5a7a2e0a0d252dfca49a965711c166f4
eron97 <eronsantosbetine@gmail.com>
2023-04-19 12:52:32 -0300

    Primeiro Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/addition', methods=['POST'])
+def addition():
+    num1 = int(request.form['num1'])
+    num2 = int(request.form['num2'])
+    result = num1 + num2
+    return render_template('result.html', result=result)
